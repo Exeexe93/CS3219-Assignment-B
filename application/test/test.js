9f1caf37e0229630dfeb3ca71882ec2e5a2806a5
Exeexe93 <e0318484@u.nus.edu>
2020-09-09 11:08:43 +0800

    Update test.js for testing on put request.

--- application/test/test.js
+++ application/test/test.js
@@ -59,7 +59,33 @@ describe("Testing the api:", () => {
                     res.body.should.be.a('object');
                     expect(contacts[0]).to.deep.equal(res.body.data);
                     done();
-                });
+                })
+        });
+
+        // Test to edit a single contact
+        it("Should edit single contact", () => {
+            // Take the first contact in the list
+            const id = contacts[0]._id;
+            chai.request(app)
+                .put(`/api/contacts/${id}`)
+                .send({
+                    'name': 'Jane Tan',
+                    'email': 'emailchange@gmail.com',
+                    'phone': '99845012',
+                    'gender': 'Female',
+                })
+                .then(function (res) {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.data._id.should.equal(id);
+                    res.body.data.name.should.equal('Jane Tan');
+                    res.body.data.email.should.equal('emailchange@gmail.com');
+                    res.body.data.phone.should.equal('99845012');
+                    res.body.data.gender.should.equal('Female');
+                })
+                .catch(function (err) {
+                    throw err;
+                })
         });
 
         // Test to remove a single contact
