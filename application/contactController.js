1787d02618713f52a044a5545c5a1cd8f033ef1b
Exeexe93 <e0318484@u.nus.edu>
2020-09-25 22:48:14 +0800

    Solved the problem resulting in error message in test

--- application/contactController.js
+++ application/contactController.js
@@ -1,80 +1,86 @@
-Contact = require('./contactModel');
+Contact = require("./contactModel");
 
 exports.index = function (req, res) {
-    Contact.get(function (err, contacts) {
-        if (err) {
-            res.json({
-                status: "Error",
-                message: err,
-            });
-        }
-        res.json({
-            status: "Success",
-            message: "Contacts retrieved successfully",
-            data: contacts
-        });
+  Contact.get(function (err, contacts) {
+    if (err) {
+      res.json({
+        status: "Error",
+        message: err,
+      });
+    }
+    res.json({
+      status: "Success",
+      message: "Contacts retrieved successfully",
+      data: contacts,
     });
+  });
 };
 
-// Handle create contact 
+// Handle create contact
 exports.new = function (req, res) {
-    var contact = new Contact();
+  var contact = new Contact();
+  contact.name = req.body.name ? req.body.name : contact.name;
+  contact.gender = req.body.gender;
+  contact.email = req.body.email;
+  contact.phone = req.body.phone;
+
+  contact.save(function (err) {
+    if (err) {
+      return res.send(err);
+    }
+    res.json({
+      message: "New contact created!",
+      data: contact,
+    });
+  });
+};
+
+// Handle view contact info
+exports.view = function (req, res) {
+  Contact.findById(req.params.contact_id, function (err, contact) {
+    if (err)
+      return res.send({
+        status: "Error",
+        message: "No such record with the contact id found!",
+      });
+    res.json({
+      message: "Loading contact details successfully",
+      data: contact,
+    });
+  });
+};
+
+// Handle update contact info
+exports.update = function (req, res) {
+  Contact.findById(req.params.contact_id, function (err, contact) {
+    if (err) return res.send(err);
     contact.name = req.body.name ? req.body.name : contact.name;
     contact.gender = req.body.gender;
     contact.email = req.body.email;
     contact.phone = req.body.phone;
 
     contact.save(function (err) {
-        res.json({
-            message: 'New contact created!',
-            data: contact
-        });
+      if (err) return res.json(err);
+      res.json({
+        message: "Contact Info updated",
+        data: contact,
+      });
     });
-};
-
-// Handle view contact info
-exports.view = function (req, res) {
-    Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'Loading contact details successfully',
-            data: contact
-        });
-    });
-};
-
-// Handle update contact info
-exports.update = function (req, res) {
-    Contact.findById(req.params.contact_id, function (err, contact) {
-        if (err)
-            res.send(err);
-        contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender;
-        contact.email = req.body.email;
-        contact.phone = req.body.phone;
-
-        // save the contact and check for errors
-        contact.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Contact Info updated',
-                data: contact
-            });
-        });
-    });
+  });
 };
 
 // Handle delete contact
 exports.delete = function (req, res) {
-    Contact.remove({
-        _id: req.params.contact_id
-    }, function (err, contact) {
-        if (err)
-            res.send(err); res.json({
-                status: "Success",
-                message: 'Contact deleted'
-            });
-    });
+  Contact.remove(
+    {
+      _id: req.params.contact_id,
+    },
+    function (err, contact) {
+      if (err) return res.send(err);
+      res.json({
+        status: "Success",
+        message: "Contact deleted",
+      });
+    }
+  );
 };
